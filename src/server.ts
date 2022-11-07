25740d98b794a22d822cf6e110f5ed393d6f377c
caosdp-rs <caosdp@gmail.com>
2022-11-07 03:48:09 -0300

    second commit3

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
+import { poolRoutes } from './routes/pool'
+import { userRoutes } from './routes/user'
+import { guessRoutes } from './routes/guess'
+import { authRoutes } from './routes/auth'
+import { gameRoutes } from './routes/game'
+
+//Singleton 
+
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
