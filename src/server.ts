2b527a8386fa36357bcbf9ec3eea78a796273da0
caosdp-rs <caosdp@gmail.com>
2022-11-07 04:14:29 -0300

    second commit13

--- src/server.ts
+++ src/server.ts
@@ -9,36 +9,30 @@ import { guessRoutes } from './routes/guess'
 import { authRoutes } from './routes/auth'
 import { gameRoutes } from './routes/game'
 
-
-// const express = require('express')
-// const app = express()
-const port = 3333
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+//Singleton 
 
 
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+async function bootStrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+    await fastify.register(cors, {
+        origin: true,
+    })
+    // Em produção isso precisa ser uma veriavel ambiente
+    await fastify.register(jwt, {
+        secret: 'Secreta0001'
+    })
+    await fastify.register(poolRoutes)
+    await fastify.register(authRoutes)   
+    await fastify.register(userRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(gameRoutes) 
+    
 
-// Run the server!
-const start = async () => {
-  try {
     await fastify.listen({ port: 3333, host: 'ip-172-31-18-111.ec2.internal' })
-    console.log(`Example app listening on port ${port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
 }
-start()
+
+
+bootStrap()
