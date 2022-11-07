7d7ba3ccfa6ba084f44f78a3da4f63b969a9f251
caosdp-rs <caosdp@gmail.com>
2022-11-07 03:56:37 -0300

    second commit5

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ fastify.get('/', async (request, reply) => {
 const start = async () => {
   try {
     await fastify.listen({ port: 3333 })
+    fastify.log.error('nada')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
