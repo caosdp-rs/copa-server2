19d97a6e5e88b5692d37c08967a6185a1383a1b3
caosdp-rs <caosdp@gmail.com>
2022-11-07 03:47:18 -0300

    second commit2

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,18 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-import { z } from 'zod'
-import ShortUniqueId from 'short-unique-id'
-import { poolRoutes } from './routes/pool'
-import { userRoutes } from './routes/user'
-import { guessRoutes } from './routes/guess'
-import { authRoutes } from './routes/auth'
-import { gameRoutes } from './routes/game'
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
 
-//Singleton 
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
 
-
-async function bootStrap() {
-    const fastify = Fastify({
-        logger: true,
-    })
-
-    await fastify.register(cors, {
-        origin: true,
-    })
-    // Em produção isso precisa ser uma veriavel ambiente
-    await fastify.register(jwt, {
-        secret: 'Secreta0001'
-    })
-    await fastify.register(poolRoutes)
-    await fastify.register(authRoutes)   
-    await fastify.register(userRoutes)
-    await fastify.register(guessRoutes)
-    await fastify.register(gameRoutes) 
-    
-
-    await fastify.listen({ port: 3333, host:'0.0.0.0' })
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
-
-
-bootStrap()
+start()
